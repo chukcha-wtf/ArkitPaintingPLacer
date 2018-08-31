a6fb1a7d7138580bd2cbdd8a8e0d2a1e905b6db8
Pavlo Babenko <pavlo.babenko@gmail.com>
2018-08-31 23:39:03 +0300

    Playing with projected position

--- App.js
+++ App.js
@@ -3,25 +3,77 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  Dimensions,
   AppRegistry,
   TouchableOpacity
 } from 'react-native';
 
-import { ARKit } from 'react-native-arkit';
+import { 
+  ARKit,
+  withProjectedPosition
+} from 'react-native-arkit';
 
 const { Box } = ARKit;
 
+const PAINTING_WIDTH = 0.5;
+const PAINTING_HEIGHT = 0.6;
+const PAINTING_THIN = 0.03;
+
+const screen = Dimensions.get('window');
+
+const Painting = withProjectedPosition()(({positionProjected, projectionResult}) => {
+  if (!projectionResult) {
+    return null;
+  }
+
+  const verticalPlaneId = projectionResult && projectionResult.id;
+
+  return (
+    <Box
+      position={positionProjected}
+      transition={{duration: 0.1}}
+      shape={{ 
+        width: PAINTING_WIDTH,
+        height: PAINTING_HEIGHT,
+        length: PAINTING_THIN
+      }}
+      doubleSided={false}
+      scale={1}
+      material={{
+        diffuse: { path: 'assets/Grant_Wood_-_American_Gothic', intensity: 1 },
+        lightingModel: ARKit.LightingModel.physicallyBased
+      }} />
+  );
+});
+
 export default class ReactNativeARKit extends Component {
   state = {
-    showNotice: true
+    showNotice: true,
+    anchors: {}
   };
 
   _hideNotice = () => {
     this.setState({showNotice: false});
   }
+
+  _onPlaneDetected = (anchor) => {
+    this._updateAnchorInfo(anchor);
+  }
+  
+  _onPlaneUpdated = (anchor) => {
+    this._updateAnchorInfo(anchor);
+  }
+  
+  _updateAnchorInfo = (anchor) => {
+    const { anchors } = this.state;
+  
+    anchors[anchor.id] = anchor;
+  
+    this.setState({anchors});
+  }
   
   render() {
-    const { showNotice } = this.state;
+    const { showNotice, anchors } = this.state;
     
     return (
       <View style={{ flex: 1 }}>
@@ -30,43 +82,17 @@ export default class ReactNativeARKit extends Component {
           debug
           planeDetection={ARKit.ARPlaneDetection.Vertical}
           lightEstimationEnabled
-          onPlaneDetected={anchor => console.log(anchor)}
-          onPlaneUpdated={anchor => console.log(anchor)}
-          onPlaneRemoved={anchor => console.log(anchor)}
-          onAnchorDetected={anchor => console.log(anchor)}
-          onAnchorUpdated={anchor => console.log(anchor)}
-          onAnchorRemoved={anchor => console.log(anchor)}
-
+          onPlaneDetected={this._onPlaneDetected}
+          onPlaneUpdated={this._onPlaneUpdated}
           onARKitError={console.log} // if arkit could not be initialized (e.g. missing permissions), you will get notified here
         >
-          <Box
-            position={{x: 0, y: 0, z: -1.5}}
-            shape={{ width: 0.5, height: 0.6, length: 0.03 }}
-            doubleSided={false}
-            scale={1}
-            material={{
-              diffuse: { path: 'assets/Grant_Wood_-_American_Gothic', intensity: 1 },
-              lightingModel: ARKit.LightingModel.physicallyBased
-            }} />
+          <Painting projectPosition={{
+              x: screen.width / 2,
+              y: screen.height / 2,
+              plane: (results) => results.length > 0 ? results[0] : null
+            }}
+          /> 
         </ARKit>
-        {
-          showNotice &&
-          <View style={{ position: 'absolute', top: 20, width: '100%', backgroundColor: 'transparent', padding: 20, alignItems: 'center', justifyContent: 'flex-start' }}>
-            <Text style={{ color: 'red', fontSize: 24 }}>
-              Please, stand in 2 meters in front of the wall
-              and restart the app.
-            </Text>
-          </View>
-        }
-        {
-          showNotice &&
-          <View style={{ position: 'absolute', width: '100%', bottom: 0, backgroundColor: 'transparent', padding: 20, alignItems: 'center', justifyContent: 'flex-end' }}>
-            <TouchableOpacity style={{ backgroundColor: 'green', alignItems: 'center', justifyContent: 'center', padding: 20 }}
-              onPress={this._hideNotice}>
-              <Text style={{ color: 'white' }}>Got it</Text>
-            </TouchableOpacity>
-          </View>
-        }
       </View>
     );
   }
