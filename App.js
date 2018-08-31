2183cfeadb9c4fd40a80f85820462aab74355b8e
Pavlo Babenko <pavlo.babenko@gmail.com>
2018-08-31 23:09:11 +0300

    Adding a friendly reminder. App is done

--- App.js
+++ App.js
@@ -1,35 +1,46 @@
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+
+import {
+  View,
+  Text,
+  AppRegistry,
+  TouchableOpacity
+} from 'react-native';
+
 import { ARKit } from 'react-native-arkit';
 
+const { Box } = ARKit;
+
 export default class ReactNativeARKit extends Component {
+  state = {
+    showNotice: true
+  };
+
+  _hideNotice = () => {
+    this.setState({showNotice: false});
+  }
+  
   render() {
+    const { showNotice } = this.state;
+    
     return (
       <View style={{ flex: 1 }}>
         <ARKit
           style={{ flex: 1 }}
           debug
-          // enable plane detection (defaults to Horizontal)
           planeDetection={ARKit.ARPlaneDetection.Vertical}
-
-          // enable light estimation (defaults to true)
           lightEstimationEnabled
-          // event listener for (horizontal) plane detection
           onPlaneDetected={anchor => console.log(anchor)}
-          // event listener for plane update
           onPlaneUpdated={anchor => console.log(anchor)}
-          // arkit sometimes removes detected planes
           onPlaneRemoved={anchor => console.log(anchor)}
-
-          // event listeners for all anchors, see [Planes and Anchors](#planes-and-anchors)
           onAnchorDetected={anchor => console.log(anchor)}
           onAnchorUpdated={anchor => console.log(anchor)}
           onAnchorRemoved={anchor => console.log(anchor)}
 
           onARKitError={console.log} // if arkit could not be initialized (e.g. missing permissions), you will get notified here
         >
-          <ARKit.Box
-            position={{x: 0, y: 0, z: -1}}
+          <Box
+            position={{x: 0, y: 0, z: -1.5}}
             shape={{ width: 0.5, height: 0.6, length: 0.03 }}
             doubleSided={false}
             scale={1}
@@ -38,6 +49,24 @@ export default class ReactNativeARKit extends Component {
               lightingModel: ARKit.LightingModel.physicallyBased
             }} />
         </ARKit>
+        {
+          showNotice &&
+          <View style={{ position: 'absolute', top: 20, width: '100%', backgroundColor: 'transparent', padding: 20, alignItems: 'center', justifyContent: 'flex-start' }}>
+            <Text style={{ color: 'red', fontSize: 24 }}>
+              Please, stand in 2 meters in front of the wall
+              and restart the app.
+            </Text>
+          </View>
+        }
+        {
+          showNotice &&
+          <View style={{ position: 'absolute', width: '100%', bottom: 0, backgroundColor: 'transparent', padding: 20, alignItems: 'center', justifyContent: 'flex-end' }}>
+            <TouchableOpacity style={{ backgroundColor: 'green', alignItems: 'center', justifyContent: 'center', padding: 20 }}
+              onPress={this._hideNotice}>
+              <Text style={{ color: 'white' }}>Got it</Text>
+            </TouchableOpacity>
+          </View>
+        }
       </View>
     );
   }
